2f9148ba36885d05819ef79ed116ef9734f0407d
Gabriel Aleks <gabriel.aleks@nano.ufrj.br>
2021-07-19 20:24:54 -0300

    Deploy website - based on ba41788546a539bbb6d4e45fafda7b61e823f321

---
+++ e28abdd9.9f4c1c78.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{144:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/avionicsdocumentation/blog/welcome"},{"title":"Hello","permalink":"/avionicsdocumentation/blog/hello-world"}]}')}}]);
