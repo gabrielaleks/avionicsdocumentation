2d19ba125644542c043439fd9d057746abd78af9
Gabriel Aleks <gabriel.aleks@nano.ufrj.br>
2021-09-09 18:44:02 -0300

    Add reuniao 0909

--- my-website/sidebars.js
+++ my-website/sidebars.js
@@ -12,7 +12,8 @@ module.exports = {
                              'reuniao/21_1/0526r', 'reuniao/21_1/0602r', 'reuniao/21_1/0609r'
                     ],
                     '2021.2': ['reuniao/21_2/0713r', 'reuniao/21_2/0719r', 'reuniao/21_2/0726r', 'reuniao/21_2/0802r',
-                               'reuniao/21_2/0809r', 'reuniao/21_2/0816r', 'reuniao/21_2/0823r', 'reuniao/21_2/0902r'
+                               'reuniao/21_2/0809r', 'reuniao/21_2/0816r', 'reuniao/21_2/0823r', 'reuniao/21_2/0902r',
+                               'reuniao/21_2/0909r'
                     ]    
                 },
             ],
